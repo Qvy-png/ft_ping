ecba796c0a3f1111afa451dd60d42cf1fa6ecbcf
Qvy <delaguilarobin5@gmail.com>
2024-04-25 11:11:50 +0200

    parsing rework

--- ft_ping.c
+++ ft_ping.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+// #include <sys/socket.h>
+// #include <netinet/in.h>
 #include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -16,6 +16,7 @@ int verbose = 0;
 
 // Calculate checksum for ICMP packet
 unsigned short checksum(void *b, int len) {
+
     unsigned short *buf = b;
     unsigned int sum = 0;
     unsigned short result;
@@ -36,6 +37,8 @@ unsigned short checksum(void *b, int len) {
 // Send ICMP echo request and wait for response
 void send_ping(int sockfd, struct sockaddr_in *addr, int seq) {
     char packet[PACKET_SIZE];
+
+    //using icmp struct from ip_icmp.h
     struct icmp *icmp_hdr;
     struct timeval start, end;
     int bytes_sent, bytes_received;
@@ -57,6 +60,7 @@ void send_ping(int sockfd, struct sockaddr_in *addr, int seq) {
     // Send ICMP packet
     bytes_sent = sendto(sockfd, packet, PACKET_SIZE, 0, (struct sockaddr *) addr, sizeof(*addr));
     if (bytes_sent <= 0) {
+
         perror("sendto");
         return;
     }
@@ -78,32 +82,67 @@ void send_ping(int sockfd, struct sockaddr_in *addr, int seq) {
         printf("Ping reply received from %s: icmp_seq=%d time=%.3f ms\n", inet_ntoa(addr->sin_addr), seq, rtt);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s [-v] <hostname or IP>\n", argv[0]);
-        return 1;
-    }
+int arg_finder(int argc, char **argv) {
 
-    int opt;
-    while ((opt = getopt(argc, argv, "v")) != -1) {
-        switch (opt) {
-            case 'v':
-                verbose = 1;
-                break;
-            default:
-                fprintf(stderr, "Usage: %s [-v] <hostname or IP>\n", argv[0]);
-                return 1;
+    int i;
+
+    i = 0;
+    while (i < argc) {
+        if (strcmp(argv[i], "-v") == 0) {
+            return 1;
         }
+        i++;
     }
+    return 0;
+}
+
+int target_finder(int argc, char **argv) {
+
+    int i;
+
+    i = 1;
+    while (i < argc) {
+        printf("Looking at target %s\n", argv[i]);
+        if (argv[i][0] == '-')
+            i++;
+        else {
+            printf("Target: %s\n", argv[i]);
+            return i;
+        }
+        // i++;
+    }
+    return -1;
+}
+
+int main(int argc, char **argv) {
 
     struct hostent *host;
     struct sockaddr_in addr;
     int sockfd, seq = 0;
 
+    printf("%d\n", argc);
+
+    if (argc < 2) {
+        printf("Usage: %s [-v] <hostname or IP>\n", argv[0]);
+        return 1;
+    }
+    if (arg_finder(argc, argv) == 1) {
+        verbose = 1;
+        printf("Verbose mode enabled\n");
+    }
+
+    int foundTarget = 0;
+    foundTarget = target_finder(argc, argv);
+    printf("Found target at index: %d\n", foundTarget);
+    if (foundTarget == -1) {
+        printf("No target found\n");
+        return 1;
+    }
+
     // Resolve hostname to IP address
-    host = gethostbyname(argv[optind]);
+    host = gethostbyname(argv[foundTarget]);
     if (host == NULL) {
-        perror("gethostbyname");
+        printf("ft_ping: cannot resolve %s: Unknown host\n", argv[foundTarget]);
         return 1;
     }
 
